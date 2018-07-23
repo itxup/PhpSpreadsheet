12c40a978fb608a39ff3ce15961e1a168f1249c6
itxup <41574081+itxup@users.noreply.github.com>
2018-07-23 12:44:38 +0200

    Quick'n dirty: make xls(x)-files readable again

--- src/PhpSpreadsheet/Shared/OLERead.php
+++ src/PhpSpreadsheet/Shared/OLERead.php
@@ -326,14 +326,14 @@ class OLERead
      */
     private static function getInt4d($data, $pos)
     {
-        if (trim($data) == '') {
+       /* if (trim($data) == '') {
             // No data provided
             throw new ReaderException('Parameter data is empty.');
         } elseif ($pos < 0) {
             // Invalid position
             throw new ReaderException('Parameter pos=' . $pos . ' is invalid.');
         }
-
+*/
         $len = strlen($data);
         if ($len < $pos + 4) {
             $data .= str_repeat("\0", $pos + 4 - $len);
